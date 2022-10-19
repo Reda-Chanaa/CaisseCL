77a192bfbf9cd59d6beeef37f238e18f6f74f3c8
Alain <77416989+darkvad@users.noreply.github.com>
2022-10-19 10:33:29 +0200

    Update prod.py

--- BackEnd/DataValidation/settings/prod.py
+++ BackEnd/DataValidation/settings/prod.py
@@ -5,6 +5,6 @@ DEBUG = False
 #SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: update this when you have the production host
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost','192.168.178.231','P-CLCAISSEAPP-A-01.corsicalinea.net']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost','192.168.178.231','P-CLYIELDAPP-A-01.corsicalinea.net']
 
 
